2a013f223274c5c1ea70c25b90e2f784b7202074
sgrill <sgrill@spartaglobal.com>
2019-11-14 17:11:55 +0000

    create a feature file and step definitions

--- MurphysLaw/src/test/java/AppTest.java
+++ MurphysLaw/src/test/java/AppTest.java
@@ -1,5 +1,3 @@
-package MurphysLaw;
-
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
---
+++ MurphysLaw/src/test/java/MyStepdefs.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("I open a website")
+    public void iOpenAWebsite() {
+    }
+
+    @When("I click on a link")
+    public void iClickOnALink() {
+    }
+
+    @Then("I am on a new webpage")
+    public void iAmOnANewWebpage() {
+    }
+}

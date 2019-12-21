e07cf76b9c635f00235b7f5ecee44f7f50554f4b
sgrill <sgrill@spartaglobal.com>
2019-12-21 12:02:29 +0000

    add chrome options to support running in linux

--- src/test/java/StepDefinitions/MyStepdefs.java
+++ src/test/java/StepDefinitions/MyStepdefs.java
@@ -8,6 +8,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class MyStepdefs {
@@ -21,8 +22,15 @@ public class MyStepdefs {
 
     @Before
     public void launchBrowser(){
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("enable-automation");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-browser-side-navigation");
+        options.addArguments("--disable-gpu");
+        webDriver = new ChromeDriver(options);
         homePage = new HomePage(webDriver);
         mensHomePage = new MensHomePage(webDriver);
     }
